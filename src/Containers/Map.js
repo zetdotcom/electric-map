b87bf66559a5b891578d112e34db6e573bdec18e
Michal Zet <michal.zadarnowski@gmail.com>
2018-01-17 01:08:29 +0000

    geolocation center

--- src/Containers/Map.js
+++ src/Containers/Map.js
@@ -1,15 +1,58 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 export class Map extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const {lat, lng} = this.props.initialCenter;
+        this.state = {
+            currentLocation: {
+                lat: lat,
+                lng: lng
+            }
+        }
+    }
 
     componentDidUpdate(prevProps, prevState) {
         if (prevProps.google !== this.props.google) {
             this.loadMap();
         }
+
+        if (prevState.currentLocation !== this.state.currentLocation) {
+            this.recenterMap();
+        }
+    }
+
+    recenterMap() {
+        const map = this.map;
+        const curr = this.state.currentLocation;
+
+        const google = this.props.google;
+        const maps = google.maps;
+
+        if (map) {
+            let center = new maps.LatLng(curr.lat, curr.lng)
+            map.panTo(center)
+        }
     }
 
     componentDidMount() {
+        if (this.props.centerAroundCurrentLocation) {
+            if (navigator && navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition((pos) => {
+                    const coords = pos.coords;
+                    this.setState({
+                        currentLocation: {
+                            lat: coords.latitude,
+                            lng: coords.longitude
+                        }
+                    })
+                })
+            }
+        }
+
         this.loadMap();
     }
 
@@ -22,9 +65,8 @@ export class Map extends React.Component {
             const mapRef = this.refs.map;
             const node = ReactDOM.findDOMNode(mapRef);
 
-            let zoom = 8;
-            let lat= 55;
-            let lng= -4;
+            let {initialCenter, zoom} = this.props;
+            const {lat, lng} = this.state.currentLocation;
             const center = new maps.LatLng(lat, lng);
             const mapConfig = {
                 center: center,
@@ -49,4 +91,21 @@ export class Map extends React.Component {
             </div>
         )
     }
+}
+
+Map.propTypes = {
+    google: PropTypes.object,
+    zoom:  PropTypes.number,
+    initialCenter:  PropTypes.object   ,
+    centerAroundCurrentLocation: PropTypes.bool
+}
+
+Map.defaultProps ={
+    zoom: 6,
+    //center of UK by default
+    initialCenter: {
+        lat: 54.621388,
+        lng: -2.592773
+    },
+    centerAroundCurrentLocation: false
 }
