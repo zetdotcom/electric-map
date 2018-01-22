00a97627116319c78723c3fec5ecc8a7f48f82a8
Michal Zet <michal.zadarnowski@gmail.com>
2018-01-22 23:03:32 +0000

    map container tidy up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import MapContainer from './Presentional/MapContainer';
-
-
-
-
 import './App.css';
-
-// import { getLocationById, getAccessible, filterByConnectorType } from './Services/PointService';
-
 
 class App extends Component {
 
-
   render() {
-// console.log(pointsService.getAccessible());
-// console.log(pointsService.getLocationById('445c4cfca9b5ba22dae9baa00e20bde5'));
-// console.log(pointsService.filterByConnectorType('Type 2 Mennekes (IEC62196)'))
     return (
       <div className="App">
-        <MapContainer />
+        <MapContainer/>
       </div>
     );
   }
--- src/Presentional/MapContainer.js
+++ src/Presentional/MapContainer.js
@@ -1,146 +1,161 @@
-import React from 'react';
-import {GoogleApiWrapper, Marker, InfoWindow} from 'google-maps-react';
-import {Map} from '../Containers/Map';
+import React from "react";
+import {
+  GoogleApiWrapper,
+  Marker,
+  InfoWindow
+} from "google-maps-react";
+import { Map } from "../Containers/Map";
 
-import * as pointsService from '../Services/PointService';
+import * as pointsService from "../Services/PointService";
 
-import './MapContainer.css';
+import "./MapContainer.css";
 
 export class MapContainer extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = {
-            showingInfoWindow: false,
-            activeMarker: {},
-            selectedPlace: {},
-            type: "all"
-        }
+      super(props);
+      this.state = {
+        showingInfoWindow: false,
+        activeMarker: {},
+        selectedPlace: {},
+        type: "all"
+      };
 
-        // binding this to event-handler functions
-        this.onMarkerClick = this
-            .onMarkerClick
-            .bind(this);
-        this.onMapClicked = this
-            .onMapClicked
-            .bind(this);
+    // binding this to event-handler functions
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClicked = this.onMapClicked.bind(this);
+    this.typeSelection = this.typeSelection.bind(this);
+  }
 
-        this.typeSelection = this
-            .typeSelection
-            .bind(this);
+  onMarkerClick(markerProps, marker, e) {
+    this.setState({
+      selectedPlace: markerProps,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
 
+  onMapClicked(props) {
+    if (this.state.showingInfoWindow) {
+      this.setState({ 
+          showingInfoWindow: false,
+          activeMarker: null
+      });
+    }
+  }
+
+  typeSelection(event) {
+    let value = event.target.value;
+
+    this.setState({
+      type: value
+    });
+  }
+
+  render() {
+    if (!this.props.loaded) {
+      return <div>Loading ...</div>;
     }
 
-    onMarkerClick(markerProps, marker, e) {
-        this.setState({selectedPlace: markerProps, activeMarker: marker, showingInfoWindow: true});
+    const style = {
+      width: "100vw",
+      height: "100vh"
+    };
+
+    const markers = pointsService
+      .filterByConnectorType(this.state.type)
+      .map((location, index) => (
+        <Marker
+          onClick={this.onMarkerClick}
+          key={location.ChargeDeviceId}
+          name={location.ChargeDeviceName}
+          position={{
+            lat: location.ChargeDeviceLocation.Latitude,
+            lng: location.ChargeDeviceLocation.Longitude
+          }}
+          buildingName={location.ChargeDeviceLocation.Address.BuildingName}
+          buildingNumber={location.ChargeDeviceLocation.Address.BuildingNumber}
+          thoroughfare={location.ChargeDeviceLocation.Address.Thoroughfare}
+          postTown={location.ChargeDeviceLocation.Address.PostTown}
+          county={location.ChargeDeviceLocation.Address.County}
+          postCode={location.ChargeDeviceLocation.Address.PostCode}
+          lat={location.ChargeDeviceLocation.Latitude}
+          lng={location.ChargeDeviceLocation.Longitude}
+          address={location.ChargeDeviceLocation.Address}
+          access24={location.Accessible24Hours}
+          payment={location.PaymentRequiredFlag}
+          payDetails={location.PaymentDetails}
+          website={location.DeviceOwner.Website}
+          connector={location.Connector.map((type, index) => {
+            return <p key={index}>{type.ConnectorType}</p>;
+          })}
+        />
+      ));
+
+      return (
+        <div style={style}>
+          <Map
+            zoom={this.state.zoom}
+            onClick={this.onMapClicked}
+            google={this.props.google}
+            typeSelection={this.typeSelection}
+          >
+            {markers}
+  
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}
+            >
+              <div className="iw">
+                <div className="iw-device-name">
+                  {this.state.selectedPlace.name}
+                </div>
+                <div className="iw-adress">
+                  <span>
+                    {this.state.selectedPlace.buildingNumber &&
+                      this.state.selectedPlace.buildingNumber}
+                    {this.state.selectedPlace.buildingName &&
+                      this.state.selectedPlace.buildingName}
+                    {this.state.selectedPlace.thoroughfare}
+                    <br /> {this.state.selectedPlace.postTown}
+                    <br /> {this.state.selectedPlace.postCode}
+                    <br /> {this.state.selectedPlace.county}
+                  </span>
+                </div>
+                <div className="iw-directions">
+                  <a
+                    href={
+                      "https://www.google.co.uk/maps/dir/Current+Location/" +
+                      this.state.selectedPlace.lat +
+                      "," +
+                      this.state.selectedPlace.lng
+                    }
+                    target="blank"
+                  >
+                    directions
+                  </a>
+                </div>
+                <div className="other-details">
+                  {this.state.selectedPlace.access24 ? <p>24hrs access</p> : ""}
+                  {/* assummed that devices with PaymentRequiredFlag are free to use */}
+                  {this.state.selectedPlace.payment
+                    ? this.state.selectedPlace.payDetails !== "" &&
+                      this.state.selectedPlace.payDetails !== null
+                      ? `cost: ${this.state.selectedPlace.payDetails}`
+                      : "FREE TO USE"
+                    : "FREE TO USE"}
+                  <br />
+                  Connectors:
+                  {this.state.selectedPlace.connector}
+                </div>
+              </div>
+            </InfoWindow>
+          </Map>
+        </div>
+      );
     }
-
-    onMapClicked(props) {
-        if (this.state.showingInfoWindow) {
-            this.setState({showingInfoWindow: false, activeMarker: null})
-        }
-        console.log(this.state)
-    }
-
-    typeSelection(event) {
-        let value = event.target.value;
-
-        this.setState({
-            type: value
-        }, () => {
-            console.log(this.state)
-        })
-    }
-
-    render() {
-        if (!this.props.loaded) {
-            return <div>Loading ...
-            </div>
-        }
-
-        const style = {
-            width: '100vw',
-            height: '100vh'
-        }
-
-        
-        const markers = pointsService
-            .filterByConnectorType(this.state.type)
-            .map((location, index) => (<Marker
-                onClick={this.onMarkerClick}
-                key={location.ChargeDeviceId}
-                name={location.ChargeDeviceName}
-                position={{
-                lat: location.ChargeDeviceLocation.Latitude,
-                lng: location.ChargeDeviceLocation.Longitude
-            }}
-                buildingName={location.ChargeDeviceLocation.Address.BuildingName}
-                buildingNumber={location.ChargeDeviceLocation.Address.BuildingNumber}
-                thoroughfare={location.ChargeDeviceLocation.Address.Thoroughfare}
-                postTown={location.ChargeDeviceLocation.Address.PostTown}
-                county={location.ChargeDeviceLocation.Address.County}
-                postCode={location.ChargeDeviceLocation.Address.PostCode}
-                lat={location.ChargeDeviceLocation.Latitude}
-                lng={location.ChargeDeviceLocation.Longitude}
-                address={location.ChargeDeviceLocation.Address}
-                access24={location.Accessible24Hours}
-                payment={location.PaymentRequiredFlag}
-                payDetails={location.PaymentDetails}
-                website={location.DeviceOwner.Website}
-                connector={location.Connector.map((type, index) => {
-                    return <p key={index}>{type.ConnectorType} </p>
-
-                console.log(location.Connector.map((type) => {
-                    console.log(type.ConnectorType)
-                }))
-                })}/>));
-
-
-        return (
-            <div style={style}>
-
-                <Map zoom={this.state.zoom} onClick={this.onMapClicked} google={this.props.google} typeSelection={this.typeSelection}>
-                    {markers}
-
-                    <InfoWindow
-                        marker={this.state.activeMarker}
-                        visible={this.state.showingInfoWindow}>
-
-                        <div className="iw">
-                            <div className="iw-device-name">{this.state.selectedPlace.name}</div>
-                            <div className="iw-adress">
-                                <span>
-                                    {this.state.selectedPlace.buildingNumber && this.state.selectedPlace.buildingNumber}
-                                    {this.state.selectedPlace.buildingName && this.state.selectedPlace.buildingName}
-                                    {this.state.selectedPlace.thoroughfare}<br/> {this.state.selectedPlace.postTown}<br/> {this.state.selectedPlace.postCode}<br/> {this.state.selectedPlace.county}
-                                </span>
-                            </div>
-                            <div className="iw-directions">
-                                <a
-                                    href={'https://www.google.co.uk/maps/dir/Current+Location/' + this.state.selectedPlace.lat + ',' + this.state.selectedPlace.lng}
-                                    target="blank">directions</a>
-                            </div>
-                            <div className="other-details">
-                                {(this.state.selectedPlace.access24)? <p>24hrs access</p> : ""}
-
-                               { /* assummed that devices with PaymentRequiredFlag are free to use */ }
-                                {(this.state.selectedPlace.payment) ?
-                                     (this.state.selectedPlace.payDetails !== "" && this.state.selectedPlace.payDetails !== null) ?
-                                      `cost: ${this.state.selectedPlace.payDetails}`:  
-                                      "FREE TO USE": "FREE TO USE"
-                                    }<br />
-                                Connectors: 
-                                {this.state.selectedPlace.connector}
-
-                                
-                                
-                                
-                            </div>
-                        </div>
-                    </InfoWindow>
-                </Map>
-            </div>
-        )
-    }
-}
-
-export default GoogleApiWrapper({apiKey: "AIzaSyB1d0hDpuBTzqNykkqMwgeJeXuOy4youM4", libraries: ['places']})(MapContainer)
+  }
+  
+  export default GoogleApiWrapper({
+    apiKey: "AIzaSyB1d0hDpuBTzqNykkqMwgeJeXuOy4youM4",
+    libraries: ["places"]
+  })(MapContainer);
