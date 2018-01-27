ddc36de76b6ffaba7348054aebb2ecb2e4b727c6
Michal Zet <michal.zadarnowski@gmail.com>
2018-01-27 01:33:30 +0000

    connector types function

--- src/Components/ConnectorTypes.js
+++ src/Components/ConnectorTypes.js
@@ -1,6 +1,27 @@
 import React from "react";
 
+import data from './../Data/data.json';
+
+
 const ConnectorTypes = (props) => {
+
+  function unique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
+  var ct = data.map((item) => {
+    return item
+      .Connector
+      .map((type) => {
+        return type.ConnectorType
+      })
+  })
+  const newArr = Array
+    .prototype
+    .concat(...ct);
+    const types = newArr.filter( unique );
+  console.log(newArr.filter( unique ));
+  
+
     return (<select className="types" onChange={props.onChange}>
     <option name="type" value="all">
       {" "}
@@ -10,35 +31,44 @@ const ConnectorTypes = (props) => {
       {" "}
       Show all{" "}
     </option>
-    <option name="type" value="3-pin Type G (BS1363)">
+    {types.map((connectorType, index) => {
+      return <option name="type" value={connectorType} key={index}>
       {" "}
-      3-pin Type G (BS1363){" "}
+      {connectorType}{" "}
     </option>
-    <option name="type" value="JEVS G105 (CHAdeMO) DC">
-      {" "}
-      JEVS G105 (CHAdeMO) DC{" "}
-    </option>
-    <option name="type" value="Type 1 SAEJ1772 (IEC 62196)">
-      {" "}
-      Type 1 SAEJ1772 (IEC 62196){" "}
-    </option>
-    <option name="type" value="Type 2 Combo (IEC62196) DC">
-      {" "}
-      Type 2 Combo (IEC62196) DC{" "}
-    </option>
-    <option name="type" value="Type 2 Mennekes (IEC62196)">
-      {" "}
-      Type 2 Mennekes (IEC62196){" "}
-    </option>
-    <option name="type" value="Type 2 Tesla (IEC62196) DC">
-      {" "}
-      Type 2 Tesla (IEC62196) DC{" "}
-    </option>
-    <option name="type" value="Type 3 Scame (IEC62196)">
-      {" "}
-      Type 3 Scame (IEC62196){" "}
-    </option>
+    })}
+    
   </select>);
 }
 
 export {ConnectorTypes};
+
+
+// <option name="type" value="3-pin Type G (BS1363)">
+//       {" "}
+//       3-pin Type G (BS1363){" "}
+//     </option>
+//     <option name="type" value="JEVS G105 (CHAdeMO) DC">
+//       {" "}
+//       JEVS G105 (CHAdeMO) DC{" "}
+//     </option>
+//     <option name="type" value="Type 1 SAEJ1772 (IEC 62196)">
+//       {" "}
+//       Type 1 SAEJ1772 (IEC 62196){" "}
+//     </option>
+//     <option name="type" value="Type 2 Combo (IEC62196) DC">
+//       {" "}
+//       Type 2 Combo (IEC62196) DC{" "}
+//     </option>
+//     <option name="type" value="Type 2 Mennekes (IEC62196)">
+//       {" "}
+//       Type 2 Mennekes (IEC62196){" "}
+//     </option>
+//     <option name="type" value="Type 2 Tesla (IEC62196) DC">
+//       {" "}
+//       Type 2 Tesla (IEC62196) DC{" "}
+//     </option>
+//     <option name="type" value="Type 3 Scame (IEC62196)">
+//       {" "}
+//       Type 3 Scame (IEC62196){" "}
+//     </option>
